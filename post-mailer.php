50c10fe3852d19f1a3126417970144c4a7b3e2d3
Jasper <j.jkuijt@gmail.com>
2023-04-22 19:12:06 +0200

    Cleaned up code
    Added some checks to ensure the e-mail function only gets fired when the post is published/updated. Also cleaned up a bit.

--- post-mailer.php
+++ post-mailer.php
@@ -31,7 +31,7 @@ add_action('init', 'pa_register_post_type');
 
 function pa_add_custom_meta_box()
 {
-    add_meta_box('pa_assigned_user', 'E-mail sturen', 'pa_render_assigned_user_meta_box', 'assigned_picture', 'side');
+    add_meta_box('pa_assigned_user', 'E-mail sturen', 'pa_render_assigned_user_meta_box', 'post_mailer', 'side');
 }
 
 add_action('add_meta_boxes', 'pa_add_custom_meta_box');
@@ -76,14 +76,20 @@ function pa_save_assigned_user_meta($post_id)
     }
 }
 
-add_action('save_post_assigned_picture', 'pa_save_assigned_user_meta');
+add_action('save_post_post_mailer', 'pa_save_assigned_user_meta');
 
 function pa_send_notification_email($post_id)
 {
+    $post_status = get_post_status($post_id);
+    if ($post_status != 'publish' && $post_status != 'future' && $post_status != 'private') {
+        return;
+    }
 
-    $user_id = get_post_meta($post_id, '_pa_assigned_user_id', true);
-    $user = get_userdata($user_id);
-    $user_email = $user->user_email;
+    $assigned_user_id = get_post_meta($post_id, '_pa_assigned_user_id', true);
+    $assigned_user = get_userdata($assigned_user_id);
+    if($assigned_user) {
+    $user_email = $assigned_user->user_email;
+    $user_name = $assigned_user->display_name;
 
     $post_password = get_post_field('post_password', $post_id);
     $post_title = get_the_title($post_id);
@@ -91,8 +97,8 @@ function pa_send_notification_email($post_id)
 
     //TODO: E-mail aanpasbaar maken met editor
     $subject = 'Er staan nieuwe foto\'s voor u klaar!';
-    $message = 'Hallo ' . $user->display_name . ',\n\n';
-    $message .= "Er staan nieuwe foto\'s voor u klaar in album $post_title!";
+    $message = "Hallo $user_name,\n\n";
+    $message .= "Er staan nieuwe foto\'s voor u klaar in album $post_title!\n";
     $message .= "Bekijk ze direct ";
     if (!empty($post_password)) {
         $message .= "met wachtwoord $post_password ";
@@ -101,7 +107,11 @@ function pa_send_notification_email($post_id)
     $message .= 'Met vriendelijke groet,\n';
     $message .= get_bloginfo('name');
 
+    $message .= $user_email . $assigned_user->display_name;
     wp_mail($user_email, $subject, $message);
+    } else {
+        error_log("unknown user was defined as e-mail recipient in post-mailer.php");
+    }
 }
-add_action('save_post', 'pa_send_notification_email');
+add_action('save_post_post_mailer', 'pa_send_notification_email');
 
